95e0dec9e5a1907840462594640634ea210ed14b
GaelCalderon <cgael5068@gmail.com>
2023-08-29 12:55:21 -0600

    se crea una tarea

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("hola!")
+// let a=2, b=5, r, s="Resultado=" 
+// r=a<<=b
+
+// const persona={
+//     nombre:"Gael",
+//     apellidoPaterno:"Calderon",
+//     apellidoMateno:"valenzuela",
+//     semestre:7,
+//     nombreCompleto: function(){
+//         return this.nombre +" "+ this.apellidoPaterno +" "+ this.apellidoMateno
+//     }
+// }
+   
+
+// const fruta = ["naranja","manzana","platano",{
+//     nombre:"Gael",
+//     apellidoPaterno:"Calderon",
+//     apellidoMateno:"valenzuela",
+//     semestre:7
+// }]
+
+
+// function promedio(u1,u2,u3,u4){
+//     let prom = (u1+u2+u3+u4)/4
+//     return prom
+// }
+// console.log(persona)
+
