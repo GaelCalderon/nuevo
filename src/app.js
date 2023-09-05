c30b58ddcbc9ec600cb56c327798f44ab25c9a24
GaelCalderon <cgael5068@gmail.com>
2023-09-05 15:32:10 -0600

    se crea router y el index.js maneja las rutas

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
-const express = require('express');
-const app = express();
+const express = require('express'); //referencia a framework express
+const app = express();//se crea la aplicacion de express
+const log = require('morgan'); //saber los clientes conectados
+const bodyParser = require('body-parser');
+const IndexRoutes = require('./routers/index.js');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000); // asigno puerto 3000
+//midelewarw utiliza morgar
+app.use(log('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+//Rutas 
+app.use('/',IndexRoutes);
+
 app.listen(app.get('port'), () => {
     console.log('El servidor esta funcionando en el puerto', app.get('port'));
 }
 
 );
+//mensaje de conectado
