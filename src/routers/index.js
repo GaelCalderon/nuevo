e7516aeae1861602289fae761c7a809ef0902bb4
GaelCalderon <cgael5068@gmail.com>
2023-09-15 09:52:35 -0600

    se modifico index.ejs

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const router =  express.Router();
-const model = require('../models/productos.js')();
-const Producto = require('../models/productos.js');
+const model = require('../models/datos.js')();
+const Producto = require('../models/datos.js');
 
 router.get('/', async (req, res) => {
-    try {
+    
         const datos = await Producto.find();
         console.log(datos);
         res.render('index.ejs', {
             datos
         });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error en el servidor');
-    }
-});
+    } 
+);
 
 module.exports = router;
