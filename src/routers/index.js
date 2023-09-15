d28479b3469c2d006f35c2055b07ae1b56bfefb5
GaelCalderon <cgael5068@gmail.com>
2023-09-15 09:25:16 -0600

    se cambio nombre de productos a datos

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,11 +3,16 @@ const router =  express.Router();
 const model = require('../models/productos.js')();
 const Producto = require('../models/productos.js');
 
-router.get('/',async(req, res) => {
-    const datos = await Producto.find();
-    console.log(datos);
-    res.render('index.ejs'),{
-        datos
+router.get('/', async (req, res) => {
+    try {
+        const datos = await Producto.find();
+        console.log(datos);
+        res.render('index.ejs', {
+            datos
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error en el servidor');
     }
 });
 
