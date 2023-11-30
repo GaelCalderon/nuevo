f27225bdded662c2a9be2c3bae75b07eaa059930
GaelCalderon <142943707+GaelCalderon@users.noreply.github.com>
2023-11-29 19:56:56 -0600

    se agrega la forma de Actulaizar

--- src/models/datos.js
+++ src/models/datos.js
@@ -6,5 +6,8 @@ const Producto = new Schema({
     nombre: String,
     precio: String
 })
+Producto.methods.update = async function (data) {
+    await this.updateOne({ _id: this._id }, data);
+};
 
 module.exports = mongoose.model('datos', Producto);
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,51 +1,60 @@
 const express = require('express');
-const router =  express.Router();
-const model = require('../models/datos.js')();
+const router = express.Router();
 const Producto = require('../models/datos.js');
 
 router.get('/', async (req, res) => {
-    
-        const datos = await Producto.find();
-        console.log(datos);
-        res.render('index.ejs', {
-            datos
-        });
-    } 
-);
-router.post('/add', async (req, res) =>{
-    const valor = new Producto(req.body);
-    console.log(req.body);
-    await valor.save();
-    res.redirect('/');
-});
-router.get('/del/:id', async(req, res) =>{
-    const {id}=req.params;
-    await Producto.findByIdAndRemove;
-    res.redirect('/');
-});
-router.get('/eliminar/:id', async (req, res) => {
     try {
-        const id = req.params.id;
-        await Producto.findByIdAndDelete(id);
+        const datos = await Producto.find();
+        console.log("los datos son", datos);
+        res.render('index.ejs', { datos });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error obteniendo los datos');
+    }
+});
+
+router.post('/add', async (req, res) => {
+    try {
+        const nuevoProducto = new Producto(req.body);
+        await nuevoProducto.save();
         res.redirect('/');
     } catch (error) {
         console.error(error);
-        res.status(500).send('Error en el servidor');
+        res.status(500).send('Error al agregar el producto');
     }
 });
 
-// Ruta para mostrar la página principal
-router.get('/', async (req, res) => {
+router.get('/del/:id', async (req, res) => {
     try {
-        const datos = await Producto.find();
-        res.render('index.ejs', {
-            datos
-        });
+        const { id } = req.params;
+        const producto = await Producto.findById(id);
+        if (!producto) {
+            return res.status(404).send('Producto no encontrado');
+        }
+        await Producto.findByIdAndRemove(id);
+        res.redirect('/');
     } catch (error) {
         console.error(error);
-        res.status(500).send('Error en el servidor');
+        res.status(500).send('Error al eliminar el producto');
     }
 });
 
+router.post('/upd/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const producto = req.body;
+        
+        const productoExistente = await Producto.findById(id);
+        if (!productoExistente) {
+            return res.status(404).send('Producto no encontrado');
+        }
+
+        await Producto.findByIdAndUpdate(id, producto);
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al actualizar el producto');
+    }
+});
 
 module.exports = router;
